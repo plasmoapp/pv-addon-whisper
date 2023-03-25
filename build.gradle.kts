0ff74c981a34ef3d473b8348ccc2c11ba7d453f5
Apehum <apehumchik@gmail.com>
2023-03-25 09:24:49 +0800

    build: cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,24 +8,10 @@ plugins {
 group = "su.plo"
 version = "1.0.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-
-    maven("https://repo.plo.su")
-}
-
 dependencies {
-    compileOnly("com.google.guava:guava:31.1-jre")
-    compileOnly("org.jetbrains:annotations:23.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.24")
-
     compileOnly("su.plo.voice.api:server:2.0.0+ALPHA")
-    compileOnly("su.plo.config:config:1.0.0")
 
     annotationProcessor("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("com.google.guava:guava:31.1-jre")
-    annotationProcessor("com.google.code.gson:gson:2.9.0")
 }
 
 plasmoCrowdin {
